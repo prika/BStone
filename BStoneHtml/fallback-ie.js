1f3cfd67bda67b9f0cd5c1e7bcb230987398340d
SofiaFerreira <prika.altx@gmail.com>
2020-03-20 10:17:44 +0000

    Minification

--- BStoneHtml/fallback-ie.js
+++ BStoneHtml/fallback-ie.js
@@ -27,18 +27,3 @@ function IEdetection() {
 }
 var result = IEdetection();
 $('html').addClass(result);
-
-
-// if($user_agent['msie']=="8.0" || $user_agent['msie']=="7.0" || $user_agent['msie']=="6.0"){
-    // if($LG=="pt"){
-    // $link_chrome = "https://www.google.com/intl/pt/chrome/browser/";
-    // $link_firefox = "http://www.mozilla.org/pt-PT/firefox/new/";
-    // $link_ie = "http://windows.microsoft.com/pt-PT/internet-explorer/download-ie";
-    // $fall_exp1 = "Verificamos que est� a utilizar uma vers�o desactualizada do Internet Explorer";
-    // $fall_exp2 = "Por favor actualize para a  
-    //               <a href='$link_ie' rel='nofollow' target='_blank'>vers�o mais recente</a>, 
-    //               ou experimente utilizar outro browser como o 
-    //               <a href='$link_chrome' rel='nofollow' target='_blank'>Google Chrome</a> ou 
-    //               <a href='$link_firefox' rel='nofollow' target='_blank'>Mozilla Firefox</a> para aproveitar todas as funcionalidades do nosso site.";
-    // }
-// }
